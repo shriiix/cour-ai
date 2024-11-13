30acf5b1ce08fb3ebfa2e05cdf45e83f52bcfbb8
shriiix <shrighadi003@gmail.com>
2024-11-14 00:34:10 +0530

    Banner

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function Header() {
   return (
-    <div>
+    <div className='flex justify-between p-5 shadow-lg'>
        <img src={'/logo.png'}width={50} height={100}></img>
+       <Button>Get Started</Button>
     </div>
   )
 }
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className="bg-gray-50">
+    <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+      <div className="mx-auto max-w-xl text-center">
+        <h1 className="text-3xl text-primary font-extrabold sm:text-5xl">
+          AI-Course Generator
+          <strong className="font-extrabold text-black sm:block"> Custom Learning Paths,Powered by AI  </strong>
+        </h1>
+  
+        <p className="mt-4 sm:text-xl/relaxed">
+        "Unlock Learning: AI-Powered Courses Curated Just for You!"
+        </p>
+  
+        <div className="mt-8 flex flex-wrap justify-center gap-4">
+          <a
+            className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+            href="#"
+          >
+            Get Started
+          </a> 
+        </div>
+      </div>
+    </div>
+  </section>
   )
 }
 
